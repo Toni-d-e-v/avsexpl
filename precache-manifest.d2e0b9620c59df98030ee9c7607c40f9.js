7b00da1dc9cdbcb7a7a02fb99faca3e21833c348
toni-dev <62844491+Toni-d-e-v@users.noreply.github.com>
2022-11-12 21:55:52 +0100

    Add files via upload

---
+++ precache-manifest.d2e0b9620c59df98030ee9c7607c40f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd76b078aa62bf1d9d5f0edbc02b99a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f30252c52e91a2970e3",
+    "url": "/static/css/main.8080d6d4.chunk.css"
+  },
+  {
+    "revision": "acf110f1dce4e0d6a8e6",
+    "url": "/static/js/2.094a67e3.chunk.js"
+  },
+  {
+    "revision": "679dc0b0298984b03ab372d214526e9a",
+    "url": "/static/js/2.094a67e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f30252c52e91a2970e3",
+    "url": "/static/js/main.bfdff844.chunk.js"
+  },
+  {
+    "revision": "ae2dfafbfc2365c4ffff",
+    "url": "/static/js/runtime-main.f7c5443e.js"
+  }
+]);
